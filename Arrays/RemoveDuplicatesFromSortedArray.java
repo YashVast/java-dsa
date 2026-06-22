c1e94344682addcc5cbcb601c903dd0eaa374d8e
YashVast <vastyash@gmail.com>
2026-06-22 13:06:35 +0530

    22-july

--- Arrays/RemoveDuplicatesFromSortedArray.java
+++ Arrays/RemoveDuplicatesFromSortedArray.java
@@ -2,28 +2,49 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class RemoveDuplicatesFromSortedArray {
+    // public static void main(String args[]){
+    //     int[] arr = {1,1,2,2,3,3,4,4,5,5};
+
+    //     int[] result = removeDuplicates(arr);
+        
+    //     for(int num:result){
+    //         System.out.print(num+" ");
+    //     }
+    // }
+    // public static int[] removeDuplicates(int[] arr){
+    //     Set<Integer> set = new HashSet<>();
+
+    //     for(int num:arr){
+    //         set.add(num);
+    //     }
+    //     int[] result = new int[set.size()];
+    //     int index = 0;
+        
+    //     for(int num:set){
+    //         result[index++] = num;
+    //     }
+    //     return result;
+    // }
+
+    //OPTIMAL --> TC: O(n) SC: O(1)
     public static void main(String args[]){
         int[] arr = {1,1,2,2,3,3,4,4,5,5};
+        int index = remove(arr);
 
-        int[] result = removeDuplicates(arr);
-        
-        for(int num:result){
-            System.out.print(num+" ");
+        for(int i=0; i<index; i++){
+            System.out.print(arr[i]+" ");
         }
     }
-    public static int[] removeDuplicates(int[] arr){
-        Set<Integer> set = new HashSet<>();
-
-        for(int num:arr){
-            set.add(num);
-        }
-        int[] result = new int[set.size()];
+    public static int remove(int[] arr){
+        int n = arr.length;
         int index = 0;
-        
-        for(int num:set){
-            result[index++] = num;
+
+        for(int i=1; i<n; i++){
+            if(arr[i]!= arr[index]){
+                arr[index+1] = arr[i];
+                index++;
+            }
         }
-        return result;
+        return index+1;
     }
-    
 }
