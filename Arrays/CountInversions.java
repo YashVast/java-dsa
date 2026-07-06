787004b602b2e399571d0f9b6e5e6da23f0dd2d4
YashVast <vastyash@gmail.com>
2026-07-06 21:32:34 +0530

    CountInversions

--- Arrays/CountInversions.java
+++ Arrays/CountInversions.java
@@ -71,9 +71,61 @@ public class CountInversions{
         }
         return count;
     }
+
+    //GLOBAL VARIABLE(AVOID IT)
+    // static int count = 0;
+    // public static void main(String args[]) {
+    //     int[] arr = {5, 3, 2, 4, 1};
+    //     System.out.println(countInversions(arr));
+    // }
+
+    // public static int countInversions(int[] arr) {
+    //     int n = arr.length;
+    //     mergeSort(arr, 0, n - 1);
+    //     return count;
+    // }
+
+    // public static void mergeSort(int[] arr, int low, int high) {
+    //     int mid = (low - high) / 2;
+    //     mergeSort(arr, low, mid);
+    //     mergeSort(arr, mid + 1, high);
+    //     merge(arr, low, mid, high);
+    // }
+
+    // public static void merge(int[] arr, int low, int mid, int high) {
+
+    //     int[] temp = new int[high - low + 1];
+
+    //     int left = low;
+    //     int right = mid + 1;
+    //     int index = 0;
+
+    //     while (left <= mid && right <= high) {
+
+    //         if (arr[left] <= arr[right]) {
+    //             temp[index++] = arr[left];
+    //             left++;
+    //         } else {
+    //             temp[index++] = arr[right];
+    //             count += mid - left + 1;
+    //             right++;
+    //         }
+    //     }
+
+    //     while (left <= mid) {
+    //         temp[index++] = arr[left];
+    //         left++;
+    //     }
+
+    //     while (right <= high) {
+    //         temp[index++] = arr[right];
+    //         right++;
+    //     }
+
+    //     for (int i = low; i <= high; i++) {
+    //         arr[i] = temp[i - low];
+    //     }
+    // }
 }
 
 
-
-//GLOBAL VARIABLE(AVOID IT)
-// static int count = 0; public static void main(String args[]){ int[] arr = {5,3,2,4,1}; System.out.println(countInversions(arr)); } public static int countInversions(int[] arr){ int n = arr.length; mergeSort(arr,0,n-1); return count; } public static void mergeSort(int[] arr, int low, int high){ int mid = (low-high)/2; mergeSort(arr, low, mid); mergeSort(arr,mid+1,high); merge(arr,low,mid,high); } public static void merge(int[] arr, int low, int mid, int high){ int[] temp = new int[high-low+1]; int left = low; int right = mid+1; int index = 0; while(left <= mid && right <= high){ if(arr[left]<= arr[right]){ temp[index++] = arr[left]; left++; } else{ temp[index++] = arr[right]; count += mid-left+1; right++; } } while(left<= mid){ temp[index++] = arr[left]; left++; } while(right<= high){ temp[index++] = arr[right]; right++; } for(int i=low; i<=high; i++){ arr[i] = temp[i-low]; } }
