3cd5480f160c910a00466f7e81eb29e7a95600aa
Yash Vast <vastyash@gmail.com>
2026-09-06 18:16:20 +0530

    MaximumConsecutiveOnes_III TC and SC updated

--- Arrays/MaximumConsecutiveOnes_III.java
+++ Arrays/MaximumConsecutiveOnes_III.java
@@ -1,4 +1,6 @@
 public class MaximumConsecutiveOnes_III {
+    //TC: O(n)+O(n) 
+    //SC: O(1)
     public static void main(String args[]){
         int[] arr = {1,1,1,0,0,0,1,1,1,1,0};
         int k = 2;
@@ -11,10 +13,12 @@ public class MaximumConsecutiveOnes_III {
         int maxCounter = 0;
         int zeroCounter = 0;
 
+        //TC: O(n)
         while(right<n){
             if(arr[right] == 0){
                 zeroCounter++;
             }
+            //TC: O(n)
             while(zeroCounter>k){
                 if(arr[left] == 0){
                     zeroCounter--;
