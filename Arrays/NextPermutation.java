2de3c7eeb3bc4249e3248e75963e25672f0b0a0b
Yash Vast <vastyash@gmail.com>
2026-09-17 07:56:30 +0530

    Implementation md added

--- Arrays/NextPermutation.java
+++ Arrays/NextPermutation.java
@@ -10,7 +10,7 @@ public class NextPermutation{
     public static int[] nextPermutation(int[] arr){
         int n = arr.length;
         int index = -1;
-        for(int i=n-2; i>-0; i--){
+        for(int i=n-2; i>=0; i--){
             if(arr[i]<arr[i+1]){
                 index = i;
                 break;
---
+++ IMPLEMENTATION.md
@@ -0,0 +1,4 @@
+1. LRU_Cache
+2. LFU_Cache
+3. HashMap_Implementation
+4. Stack_Implementation
